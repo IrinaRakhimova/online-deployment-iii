0696fd750cfbd16c4db2c80c49945c601e95b0f0
irina <rakhimovairinav@gmail.com>
2023-10-15 09:18:37 +0300

    add database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 import { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
 import * as addressService from "./services/addressService.js";
+import { sql } from "./database/database.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
